d32e30b77cc86dfe3863a79a3ea0ba6de1f79a4c
Youn <jiyoun0922@gmail.com>
2021-05-16 17:27:41 +0900

    connect to mysql

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('../config/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get(
+  '/test', (req,res,next) =>{
+    connection.query('SELECT * from user', (error, rows) => {
+      if (error) throw error;
+      console.log('User info is: ', rows);
+      res.send(rows);
+    });
+  }
+)
 module.exports = router;
